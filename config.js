aa8aed9c25d86d969f0c798d058651e9a14246dd
reza-rfdev <85278595+reza-rfdev@users.noreply.github.com>
2025-02-15 08:03:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://rezarfdev:<db_password>@rfonline.t8fhv.mongodb.net/?retryWrites=true&w=majority&appName=RFONLINE",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
